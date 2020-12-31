86693db74e18766894bcbd1a163ffd3b08497d94
yehiafouda <40678747+yehiafouda@users.noreply.github.com>
2021-01-01 01:25:27 +0200

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<?php
+include 'includes/header.html';
+?>
+
+
+<title>dashboard</title>
+</head>
+<body>
+<h1> Upload Jobs (dashboard)</h1>
+
+<?php
+include 'includes/footer.html';
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include 'includes/header.html';
+?>
+
+
+    <title>Lister Jobs</title>
+</head>
+<body>
+    <h1> View Jobs</h1>
+
+
+<?php
+include 'includes/footer.html';
+?>
